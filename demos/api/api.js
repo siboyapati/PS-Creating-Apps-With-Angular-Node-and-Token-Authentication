7a63d4c20628cad36d11f9c48826ddef19ef3451
Siva Boyapati <siva.msft@gmail.com>
2015-10-03 10:41:23 -0400

    first

---
+++ demos/api/api.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
+var passport = require('passport');
+
+var createSendToken = require('./services/jwt.js');
+var googleAuth = require('./services/googleAuth.js');
+var facebookAuth = require('./services/facebookAuth.js');
+var localStrategy = require('./services/localStrategy.js');
+var jobs = require('./services/jobs.js');
+var emailVerification = require('./services/emailVerification.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+	next();
+});
+
+passport.use('local-register', localStrategy.register);
+passport.use('local-login', localStrategy.login);
+
+app.post('/register', passport.authenticate('local-register'), function (req, res) {
+	emailVerification.send(req.user.email);
+	createSendToken(req.user, res);
+});
+
+app.get('/auth/verifyEmail', emailVerification.handler);
+
+app.post('/login', passport.authenticate('local-login'), function (req, res) {
+	createSendToken(req.user, res);
+});
+
+app.post('/auth/facebook', facebookAuth);
+
+app.get('/jobs', jobs);
+
+app.post('/auth/google', googleAuth);
+
+mongoose.connect('mongodb://localhost/psjwt');
+
+var server = app.listen(3000, function () {
+	console.log('api listening on ', server.address().port);
+});
